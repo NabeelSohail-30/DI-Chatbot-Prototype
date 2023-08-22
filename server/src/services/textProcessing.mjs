ac2f78988eda83f48187fbbe14f4a5c681a81ddc
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-22 16:22:22 +0500

    now working with loaded vector

--- server/src/services/textProcessing.mjs
+++ server/src/services/textProcessing.mjs
@@ -4,17 +4,17 @@ import { createVectorStore, loadVectorStore } from './vectorStore.mjs';
 export async function processText(docs, queryText, numOfChunks, vectorDataPath) {
 
     console.log("------------------Splitting documents------------------");
-    const splitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 1500,
-        chunkOverlap: 500,
-    });
+    // const splitter = new RecursiveCharacterTextSplitter({
+    //     chunkSize: 1500,
+    //     chunkOverlap: 500,
+    // });
 
-    const output = await splitter.splitDocuments(docs);
+    // const output = await splitter.splitDocuments(docs);
 
     console.log("------------------Documents splitted------------------");
 
     console.log("------------------Vector store------------------");
-    const vectorStore = await createVectorStore(output, vectorDataPath);
+    // const vectorStore = await createVectorStore(output, vectorDataPath);
 
     const loadedVectorStore = await loadVectorStore(vectorDataPath);
 
